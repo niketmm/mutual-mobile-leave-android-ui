899c8dfb91af773ad52f9211221bbb7fa67b3d86
Anmol Verma <anmol.verma4@gmail.com>
2022-01-04 19:31:04 +0530

    a lot of code was written by anmol!

--- app/src/main/java/com/mutualmobile/mmleave/screens/PtoScreen.kt
+++ app/src/main/java/com/mutualmobile/mmleave/screens/PtoScreen.kt
@@ -10,9 +10,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
@@ -36,21 +34,21 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.firebase.auth.FirebaseAuth
 import com.mutualmobile.mmleave.R.drawable
 import com.mutualmobile.mmleave.compose.components.TopAppBarLayout
-import com.mutualmobile.mmleave.firestore.PtoProperties
 import com.mutualmobile.mmleave.services.database.ptorequest.viewmodel.PtoRequestViewModel
 import com.mutualmobile.mmleave.ui.theme.MMLeaveTheme
 import com.mutualmobile.mmleave.ui.theme.backgroundLight
-import com.mutualmobile.mmleave.ui.theme.buttonUnselected
 import com.mutualmobile.mmleave.ui.theme.primaryColorLight
 import com.mutualmobile.mmleave.ui.theme.purpleTextColorLight
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.concurrent.TimeUnit
 
-const val DEFAULT_PATTERN = "dd/M/yyyy HH:mm:ss"
+const val DEFAULT_PATTERN = "dd/MM/yyyy HH:mm"
 
 @AndroidEntryPoint
 class PtoScreen : ComponentActivity() {
@@ -69,258 +67,134 @@ class PtoScreen : ComponentActivity() {
 }
 
 @Composable
-fun ApplyPtoScreen(
-) {
+fun ApplyPtoScreen(ptoViewModel: PtoRequestViewModel = hiltViewModel()) {
   Scaffold(
-      topBar = { TopAppBarLayout() }
+    topBar = { TopAppBarLayout() }
   ) {
     val context = LocalContext.current
-    // val email = FirebaseAuth.getInstance().currentUser?.email
-    val email = ""
     val leavesLeft = 18
-    var dateFrom: Date by remember {
-      mutableStateOf(Date())
-    }
-    var dateTo: Date by remember {
-      mutableStateOf(Date())
-    }
-    var timeFrom: String by remember {
-      mutableStateOf("")
-    }
-    var timeTo: String by remember {
-      mutableStateOf("")
-    }
-    var dateFromText by remember { mutableStateOf("") }
-    var timeFromText by remember { mutableStateOf("") }
-    var dateToText by remember { mutableStateOf("") }
-    var timeToText by remember { mutableStateOf("") }
+
+    val ptoProp = ptoViewModel.ptoRequestState.value
+
     var leaveDescriptionText by remember { mutableStateOf("") }
-    val selected = remember { mutableStateOf(false) }
-    var showDatePickerFrom by remember { mutableStateOf(false) }
-    var showTimePickerFrom by remember { mutableStateOf(false) }
-    var showDatePickerTo by remember { mutableStateOf(false) }
-    var showTimePickerTo by remember { mutableStateOf(false) }
 
-    if (showDatePickerFrom) {
-      ShowDatePicker(context = context, 0, selectDate = { date: Date, dateType: Int ->
-        dateFrom = date
-      })
-      showDatePickerFrom = false
-    }
-
-    if (showDatePickerTo) {
-      ShowDatePicker(context = context, 1,
-          selectDate = { date: Date, dateType: Int ->
-            dateTo = date
-            Log.d("PtoScreen", "ApplyPtoScreen: " + dateTo)
-          })
-      showDatePickerTo = false
-    }
-    if (showTimePickerFrom) {
-      ShowTimePicker(context = context, timeType = 0,
-          selectTime = { time: String, timeType: Int ->
-            timeFrom = time
-            Log.d("PtoScreen", "ApplyPtoScreen: " + timeFrom)
-          })
-      showTimePickerFrom = false
-    }
-
-    if (showTimePickerTo) {
-      ShowTimePicker(context = context, timeType = 1,
-          selectTime = { time: String, timeType: Int ->
-            timeTo = time
-          })
-      showTimePickerTo = false
-    }
-    if (selected.value) {
-      val formatter = SimpleDateFormat(DEFAULT_PATTERN)
-      Log.d("PtoScreen", "ApplyPtoScreen: " + timeFrom)
-      val selectedDateFrom = formatter.format(dateFrom)
-      Log.d("PtoScreen", "ShowDatePicker: $selectedDateFrom")
-
-      val selectedDateTo = formatter.parse(
-          "${dateTo.date}/${dateTo.month + 1}/${dateTo.year} $timeTo"
-      )!!
-      Log.d("PtoScreen", "ShowDatePicker: $selectedDateTo")
-      ApplyPto(
-          ptoProperties = PtoProperties(
-              selectedDateFrom, selectedDateTo, email, leaveDescriptionText
-          )
-      )
-    }
-    ConstraintLayout(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(start = 16.dp, end = 16.dp)
+    Column(
+      modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(start = 16.dp, end = 16.dp)
     ) {
-      val (selectDatesText, dateFromTf, timeFromTf, dateToTf, timeToTf, noOfLeavesLeft,
-        reasonForLeave, leaveDescription, applyPtoButton, dash) = createRefs()
+
       Text(
-          text = "Select Dates",
-          modifier = Modifier.constrainAs(selectDatesText) {
-            linkTo(top = parent.top, bottom = parent.bottom, bias = 0.05F)
-            absoluteLeft.linkTo(parent.absoluteLeft)
-          },
-          fontSize = 16.sp
+        text = "Select Dates",
+        fontSize = 16.sp
       )
-      OutlinedTextField(
-          value = dateFromText,
-          onValueChange = { dateFromText = it },
-          label = { Text("From") },
+      Column {
+        OutlinedTextField(
+          value = ptoProp.dateFrom.asFormattedString(),
+          onValueChange = { },
+          label = { Text("DateTime From") },
+          readOnly = true,
           trailingIcon = {
             Icon(
-                painter = painterResource(id = drawable.calendar_3x),
-                contentDescription = "date from text field",
-                modifier = Modifier.clickable(enabled = true, onClick = {
-                  showDatePickerFrom = true
-                }
-                ))
-          },
-          modifier = Modifier
-              .fillMaxWidth(0.25F)
-              .background(color = backgroundLight)
-              .constrainAs(dateFromTf) {
-                linkTo(top = selectDatesText.bottom, bottom = parent.bottom, bias = 0.02F)
-                absoluteRight.linkTo(timeFromTf.absoluteLeft)
-                absoluteLeft.linkTo(parent.absoluteLeft)
+              painter = painterResource(id = drawable.calendar_3x),
+              contentDescription = "date from text field",
+              modifier = Modifier.clickable(enabled = true, onClick = {
+                ShowDatePicker(context = context,
+                  selectDate = { date: Date ->
+                    ptoProp.dateFrom = date
+                    ShowTimePicker(
+                      context = context,
+                      selectTime = { time: Date ->
+                        ptoProp.dateFrom = time
+                        ptoViewModel.ptoRequestState.value.dateFrom = time
+                      }, ptoProp.dateFrom
+                    )
+                  })
               }
-      )
-
-      OutlinedTextField(
-          value = timeFromText,
-          onValueChange = { timeFromText = it },
-          label = { Text("Time From") },
-          trailingIcon = {
-            Icon(
-                painter = painterResource(id = drawable.ic_baseline_access_time_24),
-                contentDescription = "time from text field",
-                modifier = Modifier.clickable(enabled = true, onClick = {
-                  showTimePickerFrom = true
-                }
-                ))
+              ))
           },
           modifier = Modifier
-              .fillMaxWidth(0.25F)
-              .background(color = backgroundLight)
-              .constrainAs(timeFromTf) {
-                linkTo(top = selectDatesText.bottom, bottom = parent.bottom, bias = 0.02F)
-                absoluteRight.linkTo(dash.absoluteLeft)
-                absoluteLeft.linkTo(dateFromTf.absoluteRight, 5.dp)
+            .fillMaxWidth()
+            .background(color = backgroundLight)
+        )
+
+        OutlinedTextField(
+          value = ptoProp.dateTo.asFormattedString(),
+          onValueChange = { },
+          label = { Text("DateTime To") },
+          readOnly = true,
+          trailingIcon = {
+            Icon(
+              painter = painterResource(id = drawable.ic_baseline_access_time_24),
+              contentDescription = "time from text field",
+              modifier = Modifier.clickable(enabled = true, onClick = {
+                ShowDatePicker(context = context,
+                  selectDate = { date: Date ->
+                    ptoProp.dateTo = date
+                    ShowTimePicker(
+                      context = context,
+                      selectTime = { time: Date ->
+                        ptoProp.dateTo = time
+                        ptoViewModel.ptoRequestState.value.dateTo = time
+                      }, ptoProp.dateTo
+                    )
+                  })
               }
+              ))
+          },
+          modifier = Modifier
+            .fillMaxWidth()
+            .background(color = backgroundLight)
+
+        )
+      }
+
+      Text(
+        "-",
+        modifier = Modifier
+          .fillMaxWidth(0.05F),
+        textAlign = TextAlign.Center
       )
 
       Text(
-          "-",
-          modifier = Modifier
-              .fillMaxWidth(0.05F)
-              .constrainAs(dash) {
-                linkTo(top = selectDatesText.bottom, bottom = parent.bottom, bias = 0.05F)
-                absoluteLeft.linkTo(timeFromTf.absoluteRight)
-                absoluteRight.linkTo(dateToTf.absoluteLeft)
-              },
-          textAlign = TextAlign.Center
-      )
-
-      OutlinedTextField(
-          value = dateToText,
-          onValueChange = { dateToText = it },
-          label = { Text("To") },
-          trailingIcon = {
-            Icon(
-                painter = painterResource(id = drawable.calendar_3x),
-                contentDescription = "date to text field",
-                modifier = Modifier.clickable(enabled = true, onClick = {
-                  showDatePickerTo = true
-                }
-                ))
-          },
-          modifier = Modifier
-              .fillMaxWidth(0.25F)
-              .background(color = backgroundLight)
-              .constrainAs(dateToTf) {
-                linkTo(top = selectDatesText.bottom, bottom = parent.bottom, bias = 0.02F)
-                absoluteLeft.linkTo(dash.absoluteRight)
-                absoluteRight.linkTo(timeToTf.absoluteLeft, 5.dp)
-              },
-
-          )
-
-      OutlinedTextField(
-          value = timeToText,
-          onValueChange = { timeToText = it },
-          label = { Text("Time To") },
-          trailingIcon = {
-            Icon(
-                painter = painterResource(id = drawable.ic_baseline_access_time_24),
-                contentDescription = "time to text field",
-                modifier = Modifier.clickable(enabled = true, onClick = {
-                  showTimePickerTo = true
-                }
-                ))
-          },
-          modifier = Modifier
-              .fillMaxWidth(0.25F)
-              .background(color = backgroundLight)
-              .constrainAs(timeToTf) {
-                linkTo(top = selectDatesText.bottom, bottom = parent.bottom, bias = 0.02F)
-                absoluteLeft.linkTo(dateToTf.absoluteRight)
-                absoluteRight.linkTo(parent.absoluteRight)
-              },
-
-          )
-      Text(
-          text = "Leaves Left: $leavesLeft",
-          modifier = Modifier.constrainAs(noOfLeavesLeft) {
-            linkTo(top = dateToTf.bottom, bottom = parent.bottom, bias = 0.03F)
-            absoluteRight.linkTo(parent.absoluteRight)
-          },
-          color = purpleTextColorLight,
-          fontSize = 16.sp
+        text = "Leaves Left: $leavesLeft",
+        color = purpleTextColorLight,
+        fontSize = 16.sp
       )
 
       Text(
-          text = "Add Reason Of Leave (Optional)",
-          modifier = Modifier.constrainAs(reasonForLeave) {
-            top.linkTo(noOfLeavesLeft.bottom)
-            linkTo(top = noOfLeavesLeft.bottom, bottom = parent.bottom, bias = 0.05F)
-            absoluteLeft.linkTo(parent.absoluteLeft)
-          },
-          fontSize = 16.sp
+        text = "Add Reason Of Leave (Optional)",
+        fontSize = 16.sp
       )
       OutlinedTextField(
-          value = leaveDescriptionText,
-          onValueChange = {
-            leaveDescriptionText = it
-          },
-          modifier = Modifier
-              .fillMaxWidth()
-              .background(color = backgroundLight)
-              .constrainAs(leaveDescription) {
-                linkTo(top = reasonForLeave.bottom, bottom = parent.bottom, bias = 0.03F)
-                absoluteLeft.linkTo(parent.absoluteLeft)
-              },
-          maxLines = 5,
+        value = leaveDescriptionText,
+        onValueChange = {
+          leaveDescriptionText = it
+        },
+        modifier = Modifier
+          .fillMaxWidth()
+          .background(color = backgroundLight),
+        maxLines = 5,
       )
       Button(
-          onClick = { selected.value = !selected.value },
-          colors = ButtonDefaults.buttonColors(
-              backgroundColor = if (selected.value)
-                primaryColorLight else buttonUnselected,
-          ),
-          modifier = Modifier.constrainAs(applyPtoButton) {
-            linkTo(top = leaveDescription.bottom, bottom = parent.bottom, bias = 0.8F)
-            absoluteLeft.linkTo(parent.absoluteLeft)
-            absoluteRight.linkTo(parent.absoluteRight)
-          },
-
-          ) {
+        onClick = {
+          requestPtoNow(
+            ptoViewModel,
+            FirebaseAuth.getInstance().currentUser?.email!!,
+            leaveDescriptionText
+          )
+        },
+        colors = ButtonDefaults.buttonColors(
+          backgroundColor = primaryColorLight,
+        ),
+      ) {
         Text(
-            text = "APPLY PTO",
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp,
-            color = if (selected.value) Color.White else Color.Black
+          text = "APPLY PTO",
+          modifier = Modifier.fillMaxWidth(),
+          textAlign = TextAlign.Center,
+          fontSize = 20.sp,
+          color = Color.White
         )
       }
     }
@@ -328,42 +202,45 @@ fun ApplyPtoScreen(
   }
 }
 
-@Composable
-fun ApplyPto(
-  ptoProperties: PtoProperties,
-) {
-  val ptoViewModel: PtoRequestViewModel = hiltViewModel()
-  ptoViewModel.applyPtoRequest(ptoProperties)
+private fun Date?.asFormattedString(): String {
+  val formatter = SimpleDateFormat(DEFAULT_PATTERN)
+  return formatter.format(this)
 }
 
-@Composable
+private fun requestPtoNow(
+  ptoViewModel: PtoRequestViewModel,
+  email: String,
+  leaveDescriptionText: String
+) {
+  ptoViewModel.applyPtoRequest(email, leaveDescriptionText)
+}
+
 fun ShowTimePicker(
   context: Context,
-  timeType: Int,
-  selectTime: (String, timeType: Int) -> Unit
+  selectTime: (Date) -> Unit,
+  date: Date?
 ) {
   val mMinute: Int
   val mHour: Int
-  val mSecond: Int
   val now = Calendar.getInstance()
   mMinute = now.get(Calendar.MINUTE)
-  mHour = now.get(Calendar.HOUR)
-  now.time = Date()
+  mHour = now.get(Calendar.HOUR_OF_DAY)
+  now.time = date
 
   val timePickerDialog = TimePickerDialog(
-      context,
-      { view, hourOfDay, minute ->
-        selectTime("$hourOfDay:$minute:00", timeType)
-      }, mHour, mMinute, true
+    context,
+    { view, hourOfDay, minute ->
+      now.set(Calendar.HOUR_OF_DAY,hourOfDay)
+      now.set(Calendar.MINUTE,minute)
+      selectTime(now.time)
+    }, mHour, mMinute, true
   )
   timePickerDialog.show()
 }
 
-@Composable
 fun ShowDatePicker(
   context: Context,
-  dateType: Int,
-  selectDate: (Date, dateType: Int) -> Unit,
+  selectDate: (Date) -> Unit,
 ) {
   val mYear: Int
   val mMonth: Int
@@ -373,19 +250,21 @@ fun ShowDatePicker(
   mMonth = now.get(Calendar.MONTH)
   mDay = now.get(Calendar.DAY_OF_MONTH)
   now.time = Date()
-  val dateSelected = remember { mutableStateOf(Date()) }
   val datePickerDialog = DatePickerDialog(
-      context,
-      { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
-        val cal = Calendar.getInstance()
-        cal.set(year, month, dayOfMonth)
-      }, mYear, mMonth, mDay
+    context,
+    { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+      val cal = Calendar.getInstance()
+      cal.set(year, month, dayOfMonth)
+    }, mYear, mMonth, mDay
   )
 
-  datePickerDialog.setOnDateSetListener { datePicker, date, month, year ->
-    dateSelected.value = Date(date, month, year)
-    selectDate(dateSelected.value, dateType)
-    Log.d("PtoScreen", "ShowDatePicker: " + dateSelected.value)
+  datePickerDialog.setOnDateSetListener { datePicker, year, month, date ->
+    // Create a Calendar instance
+    val mCalendar = Calendar.getInstance()
+    mCalendar[Calendar.YEAR] = year
+    mCalendar[Calendar.MONTH] = month
+    mCalendar[Calendar.DAY_OF_MONTH] = date
+    selectDate(mCalendar.time)
   }
   datePickerDialog.show()
 }
--- app/src/main/java/com/mutualmobile/mmleave/services/database/ptorequest/viewmodel/PtoRequestViewModel.kt
+++ app/src/main/java/com/mutualmobile/mmleave/services/database/ptorequest/viewmodel/PtoRequestViewModel.kt
@@ -1,22 +1,33 @@
 package com.mutualmobile.mmleave.services.database.ptorequest.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mutualmobile.mmleave.firestore.PtoProperties
 import com.mutualmobile.mmleave.services.database.ptorequest.PtoRequestService
+import com.mutualmobile.mmleave.services.database.ptorequest.PtoRequestServiceImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PtoRequestViewModel @Inject constructor(private val ptoRequestService: PtoRequestService) :
-    ViewModel() {
+class PtoRequestViewModel @Inject constructor(private val ptoRequestService: PtoRequestServiceImpl) :
+  ViewModel() {
+
+  var ptoRequestState = mutableStateOf(PtoProperties())
+    private set
 
   fun applyPtoRequest(
-    ptoProperties: PtoProperties
+    email: String,
+    leaveDescriptionText: String
   ) {
     viewModelScope.launch {
-      ptoRequestService.makePtoRequest(ptoProperties)
+      ptoRequestService.makePtoRequest(
+        ptoRequestState.value.copy(
+          email = email,
+          description = leaveDescriptionText
+        )
+      )
     }
   }
 }
