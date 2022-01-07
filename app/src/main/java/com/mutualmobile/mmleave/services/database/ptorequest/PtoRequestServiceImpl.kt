0e81ec798fa6bc9c17d5c02f70c7f239523229c0
Nikhil Chauhan <nikhil.chauhan@mutualmobile.com>
2022-01-07 13:40:15 +0530

    firestore changes

--- app/src/main/java/com/mutualmobile/mmleave/services/database/ptorequest/PtoRequestServiceImpl.kt
+++ app/src/main/java/com/mutualmobile/mmleave/services/database/ptorequest/PtoRequestServiceImpl.kt
@@ -4,10 +4,8 @@ import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.mutualmobile.mmleave.firestore.PtoRequest
-import com.mutualmobile.mmleave.services.database.ptorequest.querypto.filterDuplicatePtos
-import com.mutualmobile.mmleave.services.database.ptorequest.querypto.getAllPtoRequests
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.mutualmobile.mmleave.services.database.user.USERS_LIST_COLLECTION
+import java.time.LocalDate
 
 const val PTO_LIST_COLLECTION = "PTOs_list"
 
@@ -19,18 +17,15 @@ class PtoRequestServiceImpl : PtoRequestService {
     val pto = HashMap<String, Any?>()
     pto["description"] = ptoRequest.description
     pto["email"] = ptoRequest.email
-    var ptosList = listOf<FirebasePtoRequest>()
-    // GlobalScope.launch {
-    //   ptosList = getAllPtoRequests(ptoRequest)
-    // }.join()
-    // val list =filterDuplicatePtos(ptosList, ptoRequest)
+    pto["pto_list"] = ptoRequest.ptoList
 
-    // FirebaseAuth.getInstance().currentUser?.email?.let { safeEmail ->
-    //   FirebaseFirestore.getInstance()
-    //       .collection(USERS_LIST_COLLECTION)
-    //       .document(safeEmail)
-    //       .collection(PTO_LIST_COLLECTION)
-    //       .add(ptoRequest)
+    FirebaseAuth.getInstance().currentUser?.email?.let { safeEmail ->
+      FirebaseFirestore.getInstance()
+          .collection(USERS_LIST_COLLECTION)
+          .document(safeEmail)
+          .collection(PTO_LIST_COLLECTION)
+          .add(ptoRequest)
+    }
   }
 
   override suspend fun approvePtoRequest(
