e0f25d8555d2f3f714cbbb3635f4744e0d5da864
niketmm <niket.jain@mutualmobile.com>
2022-01-10 00:36:39 +0530

    added: Multiple user selected feature

--- app/src/main/java/com/mutualmobile/mmleave/data/model/MMUser.kt
+++ app/src/main/java/com/mutualmobile/mmleave/data/model/MMUser.kt
@@ -2,6 +2,7 @@ package com.mutualmobile.mmleave.data.model
 
 data class MMUser(
     var displayName : String? = "",
+    var designation : String? = "employee",
     var email : String? = "",
     var nameAsArrays : ArrayList<String>? = null,
     var photoUrl : String? = "",
@@ -9,4 +10,5 @@ data class MMUser(
     var leaveTaken : Int? = 0,
     var totalLeave : Int? = 20,
     var leaveLeft : Int? = 20,
+    var isSelected : Boolean? = true
 )
--- app/src/main/java/com/mutualmobile/mmleave/screens/search/SearchComponent.kt
+++ app/src/main/java/com/mutualmobile/mmleave/screens/search/SearchComponent.kt
@@ -1,5 +1,8 @@
 package com.mutualmobile.mmleave.screens.search
 
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -17,15 +21,18 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +42,6 @@ import androidx.navigation.compose.rememberNavController
 import coil.annotation.ExperimentalCoilApi
 import com.mutualmobile.mmleave.data.model.MMUser
 import com.mutualmobile.mmleave.screens.ProfileImageHolder
-import com.mutualmobile.mmleave.services.database.ptorequest.viewmodel.PtoRequestViewModel
 import com.mutualmobile.mmleave.services.database.search_user.SearchUserViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -47,10 +53,17 @@ fun SearchScreen(
 ) {
     // This contains the Search View and the Lazy Column with the details
     val textState = remember { mutableStateOf(TextFieldValue("Search Admins by here")) }
+
+    val ptoAppliedState = remember { mutableStateOf(false) }
+
     val adminListState = viewModel.adminListState.value
+
     var filteredList: List<MMUser?>
 
     val searchedText = textState.value.text
+
+    val TAG = "Testing"
+
     Column(modifier = Modifier.padding(8.dp)) {
         SearchViewComposable(state = textState, viewModel)
         Spacer(modifier = Modifier.height(16.dp))
@@ -62,8 +75,41 @@ fun SearchScreen(
             }
 
             items(filteredList) { admin ->
-                ColumnCardViewComposable(firebaseAdminUser = admin)
+                Row(
+                    modifier = Modifier
+                        .clickable {
+                            val selectedUser = filteredList[filteredList.indexOf(admin)]
+                            selectedUser?.copy(isSelected = selectedUser.isSelected?.not())
+                            Log.d(TAG, "SearchScreen: ${selectedUser?.isSelected}")
+                        }
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ColumnCardViewComposable(firebaseAdminUser = admin)
+                    if (admin?.isSelected == true) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = "item_selected",
+                            modifier = Modifier.size(16.dp)
+                        )
+                    }
+                }
             }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                ptoAppliedState.value = !ptoAppliedState.value
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Send Application",
+                color = MaterialTheme.colors.secondaryVariant,
+                fontSize = 18.sp,
+                fontStyle = FontStyle.Normal
+            )
         }
     }
 }
@@ -129,20 +175,22 @@ fun SearchViewComposable(
 @ExperimentalCoilApi
 @Composable
 fun ColumnCardViewComposable(firebaseAdminUser: MMUser?) {
-    // This will display data in the card
-    // Todo Change the Parameter later
     Card(
         shape = MaterialTheme.shapes.small,
         modifier = Modifier
-            .padding(4.dp)
-            .fillMaxWidth(),
+            .size(width = 240.dp, height = 40.dp)
+            .padding(4.dp),
+//            .fillMaxWidth(),
         elevation = 4.dp
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 8.dp)
+//                .fillMaxWidth()
+                .padding(all = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
+
             firebaseAdminUser?.photoUrl?.let {
                 ProfileImageHolder(
                     rememberNavController(),
@@ -150,15 +198,28 @@ fun ColumnCardViewComposable(firebaseAdminUser: MMUser?) {
                 )
             }
 
-            firebaseAdminUser?.let {
-                Text(
-                    text = it.displayName ?: "Empty",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(4.dp),
-                    color = Color.Black,
-                    fontSize = 16.sp
-                )
+            Column(modifier = Modifier.padding(start = 8.dp)) {
+                firebaseAdminUser?.let {
+                    Text(
+                        text = it.displayName ?: "Empty",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(4.dp),
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    )
+                }
+
+                firebaseAdminUser?.let {
+                    Text(
+                        text = it.designation ?: "Empty",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 4.dp),
+                        color = Color.Black,
+                        fontSize = 12.sp
+                    )
+                }
             }
         }
     }
