66f0156a77a0d9af8784cdf3b9be8692c1e7758d
niketmm <niket.jain@mutualmobile.com>
2022-01-11 01:25:18 +0530

    added : FirebaseCollectionRef and interface's module

---
+++ app/src/main/java/com/mutualmobile/mmleave/di/FirebaseModule.kt
@@ -0,0 +1,40 @@
+package com.mutualmobile.mmleave.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.mutualmobile.mmleave.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    @Named("firebaseUserCollectionReference")
+    fun provideFirebaseUserCollectionReference() : CollectionReference {
+        return FirebaseFirestore.getInstance()
+            .collection(Constants.USERS_LIST_COLLECTION)
+    }
+
+    @Provides
+    @Singleton
+    @Named("firebasePtoRequestCollectionReference")
+    fun provideUserPtoRequestDocReference() : CollectionReference {
+        val userEmail = FirebaseAuth.getInstance()
+            .currentUser
+            ?.email ?: " "
+
+        return FirebaseFirestore.getInstance()
+            .collection(Constants.USERS_LIST_COLLECTION)
+            .document(userEmail)
+            .collection(Constants.PTO_LIST_COLLECTION)
+    }
+}
---
+++ app/src/main/java/com/mutualmobile/mmleave/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.mutualmobile.mmleave.util
+
+object Constants {
+    const val PTO_LIST_COLLECTION = "pto_requests"
+    const val USERS_LIST_COLLECTION = "users_list"
+
+}
