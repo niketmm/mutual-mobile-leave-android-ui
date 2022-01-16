91a1f384ef54ed095e12f96c1ed5d340e22fb481
niketmm <niket.jain@mutualmobile.com>
2022-01-17 00:03:53 +0530

    fixed #57

--- app/src/main/java/com/mutualmobile/mmleave/screens/home/HomeScreen.kt
+++ app/src/main/java/com/mutualmobile/mmleave/screens/home/HomeScreen.kt
@@ -28,15 +28,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -59,250 +56,179 @@ fun HomeScreen(
     navController: NavHostController,
     homeScreenViewModel: HomeScreenViewModel = hiltViewModel()
 ) {
-    val constraints = ConstraintSet {
-        val topProfileAndGreetingLayout = createRefFor("topProfileAndGreetingLayout")
-        val topCalendarViewLayout = createRefFor("topCalendarViewLayout")
-        val calendarDetailViewLayout = createRefFor("calendarDetailViewLayout")
-        val upperMidTotalLeaveLayout = createRefFor("upperMidTotalLeaveLayout")
-        val middleLine = createRefFor("middleLine")
-        val lowerMidAppliedLeaveLayout = createRefFor("lowerMidAppliedLeaveLayout")
-        val lowerFooterLayout = createRefFor("lowerFooterLayout")
-        val lowerFooterButton = createRefFor("lowerFooterButton")
-
-//        val midGuideline = createGuidelineFromTop(0.7f)
-//        val verticalMidGuideline = createGuidelineFromAbsoluteRight(0.5f)
-
-        constrain(topProfileAndGreetingLayout) {
-            top.linkTo(parent.top)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }
-
-        constrain(topCalendarViewLayout) {
-            top.linkTo(topProfileAndGreetingLayout.bottom)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }
-
-        constrain(calendarDetailViewLayout) {
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            top.linkTo(topCalendarViewLayout.bottom)
-        }
-
-        constrain(upperMidTotalLeaveLayout) {
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            top.linkTo(calendarDetailViewLayout.bottom)
-            bottom.linkTo(middleLine.top)
-        }
-
-        constrain(middleLine) {
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            top.linkTo(upperMidTotalLeaveLayout.bottom)
-            bottom.linkTo(lowerMidAppliedLeaveLayout.top)
-        }
-
-        constrain(lowerMidAppliedLeaveLayout) {
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            top.linkTo(middleLine.bottom)
-        }
-
-        constrain(lowerFooterLayout) {
-            start.linkTo(parent.start)
-            bottom.linkTo(parent.bottom)
-        }
-
-        constrain(lowerFooterButton) {
-            start.linkTo(parent.absoluteLeft)
-            end.linkTo(parent.end)
-            bottom.linkTo(parent.bottom)
-        }
-
-    }
-
     val scrollableState = rememberScrollState()
 
-    LaunchedEffect(Unit) { scrollableState.animateScrollTo(10000) }
+    LaunchedEffect(Unit) { scrollableState.animateScrollTo(0) }
 
-    ConstraintLayout(constraintSet = constraints, modifier = Modifier.wrapContentHeight()) {
-        Column(
+    Column(
+        modifier = Modifier
+            .wrapContentHeight()
+            .verticalScroll(scrollableState),
+    ) {
+        Spacer(modifier = Modifier.width(24.dp))
+        Row(
             modifier = Modifier
-                .wrapContentHeight()
-                .verticalScroll(scrollableState),
+                .fillMaxWidth()
+                .padding(start = 24.dp, end = 24.dp, top = 8.dp, bottom = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Spacer(modifier = Modifier.width(24.dp))
+            val name = FirebaseAuth.getInstance().currentUser?.displayName.toString()
+            Column {
+                Text(
+                    text = stringResource(R.string.good_morning_home_screen_text),
+                    fontSize = 16.sp,
+                    color = secondaryTextColorDark
+                )
+                Text(text = name, fontSize = 24.sp)
+            }
+
+            Row(horizontalArrangement = Arrangement.End) {
+                OutlineCalendarButton(navController)
+                Spacer(modifier = Modifier.width(8.dp))
+                ProfileImageHolder()
+            }
+        }
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Box(
+            modifier = Modifier
+                .padding(bottom = 12.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            StaticHomeCalendar(homeScreenViewModel)
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+            ) {
+                CalendarDetailsCard()
+            }
+        }
+
+        Surface(
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp, top = 8.dp, bottom = 8.dp)
+                .height(158.dp)
+                .clickable { navController.navigate(Screen.Splash.route) },
+            shape = MaterialTheme.shapes.large,
+            color = primaryColorLight,
+
+            ) {
             Row(
                 modifier = Modifier
-                    .layoutId("topProfileAndGreetingLayout")
-                    .fillMaxWidth()
-                    .padding(start = 24.dp, end = 24.dp, top = 8.dp, bottom = 8.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                val name = FirebaseAuth.getInstance().currentUser?.displayName.toString()
-                Column {
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.Start,
+                    modifier = Modifier
+                        .padding(start = 24.dp)
+                ) {
+                    Text(text = "18 of 24", fontSize = 40.sp, color = Color.White)
                     Text(
-                        text = stringResource(R.string.good_morning_home_screen_text),
-                        fontSize = 16.sp,
+                        text = "PTOs availed",
+                        fontSize = 20.sp,
                         color = secondaryTextColorDark
                     )
-                    Text(text = name, fontSize = 24.sp)
+                    Text(text = "SEE DETAILS -->", fontSize = 16.sp, color = Color.White)
                 }
 
-                Row(horizontalArrangement = Arrangement.End) {
-                    OutlineCalendarButton(navController)
-                    Spacer(modifier = Modifier.width(8.dp))
-                    ProfileImageHolder()
-                }
-            }
-
-            Spacer(modifier = Modifier.width(12.dp))
-
-            Box(
-                modifier = Modifier
-                    .layoutId("topCalendarViewLayout")
-                    .padding(bottom = 12.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                StaticHomeCalendar(homeScreenViewModel)
-            }
-
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
                 Box(
-                    modifier = Modifier
-                        .layoutId("calendarDetailViewLayout")
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxWidth()
                 ) {
-                    CalendarDetailsCard()
-                }
-            }
-
-            Surface(
-                modifier = Modifier
-                    .layoutId("upperMidTotalLeaveLayout")
-                    .padding(start = 24.dp, end = 24.dp, top = 8.dp, bottom = 8.dp)
-                    .height(158.dp)
-                    .clickable { navController.navigate(Screen.Splash.route) },
-                shape = MaterialTheme.shapes.large,
-                color = primaryColorLight,
-
-                ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Column(
-                        verticalArrangement = Arrangement.SpaceEvenly,
-                        horizontalAlignment = Alignment.Start,
-                        modifier = Modifier
-                            .padding(start = 24.dp)
-                    ) {
-                        Text(text = "18 of 24", fontSize = 40.sp, color = Color.White)
-                        Text(
-                            text = "PTOs availed",
-                            fontSize = 20.sp,
-                            color = secondaryTextColorDark
-                        )
-                        Text(text = "SEE DETAILS -->", fontSize = 16.sp, color = Color.White)
-                    }
-
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        LeaveAnimatedCircularProgressBar(
-                            percentage = 0.77f,
-                            totalValue = 100
-                        )
-                    }
-                }
-            }
-
-            // Showing a line
-            Box(
-                modifier = Modifier
-                    .layoutId("middleLine")
-                    .fillMaxWidth()
-                    .padding(24.dp)
-                    .height(1.dp)
-                    .background(color = secondaryTextColorDark)
-            ) {
-                // This is just a view (Line)
-            }
-
-            Column(
-                modifier = Modifier
-                    .layoutId("lowerMidAppliedLeaveLayout")
-                    .padding(start = 24.dp, end = 24.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(
-                    text = stringResource(R.string.applied_pto_heading_home_screen),
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
-                Surface(
-                    modifier = Modifier
-                        .clickable { navController.navigate(Screen.PtoRequests.route) }
-                        .fillMaxWidth()
-                        .height(120.dp),
-                    shape = MaterialTheme.shapes.large,
-                    color = Color.White
-                ) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 4.dp, end = 4.dp),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            Text(text = "Feb 20, 2021 - Feb 25, 2021 ", fontSize = 14.sp)
-                            Text(text = "Approved", fontSize = 18.sp)
-                        }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        ExpandingText(text = AnnotatedString(text = stringResource(id = R.string.long_text)))
-                    }
-                }
-            }
-
-            Column(
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.Start
-            ) {
-                Box {
-                    Image(
-                        painterResource(id = R.drawable.home_page_illus_mm_leave),
-                        contentDescription = stringResource(R.string.bottom_home_img_illustration),
-                        modifier = Modifier
-                            .height(200.dp)
-                            .width(270.dp),
+                    LeaveAnimatedCircularProgressBar(
+                        percentage = 0.77f,
+                        totalValue = 100
                     )
                 }
             }
+        }
 
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.End
+        // Showing a line
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+                .height(1.dp)
+                .background(color = secondaryTextColorDark)
+        ) {
+            // This is just a view (Line)
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.applied_pto_heading_home_screen),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Surface(
+                modifier = Modifier
+                    .clickable { navController.navigate(Screen.PtoRequests.route) }
+                    .fillMaxWidth()
+                    .height(120.dp),
+                shape = MaterialTheme.shapes.large,
+                color = Color.White
             ) {
-                Box(
-                    modifier = Modifier
-                        .padding(bottom = 32.dp)
-                ) {
-                    Button(onClick = { navController.navigate(Screen.ApplyPto.route) }) {
-                        Text(text = "APPLY PTO")
-                        Icon(
-                            imageVector = Icons.Default.ArrowForward,
-                            contentDescription = stringResource(R.string.forward_arrow_icon_desc)
-                        )
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 4.dp, end = 4.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text(text = "Feb 20, 2021 - Feb 25, 2021 ", fontSize = 14.sp)
+                        Text(text = "Approved", fontSize = 18.sp)
                     }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    ExpandingText(text = AnnotatedString(text = stringResource(id = R.string.long_text)))
                 }
+            }
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.End
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(bottom = 32.dp),
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                Button(onClick = { navController.navigate(Screen.ApplyPto.route) }) {
+                    Text(text = "APPLY PTO")
+                    Icon(
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = stringResource(R.string.forward_arrow_icon_desc)
+                    )
+                }
+            }
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Box {
+                Image(
+                    painterResource(id = R.drawable.home_page_illus_mm_leave),
+                    contentDescription = stringResource(R.string.bottom_home_img_illustration),
+                    modifier = Modifier
+                        .height(200.dp)
+                        .width(270.dp),
+                )
             }
         }
     }
