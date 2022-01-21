a382f5a4095cd96c6d8977d352a805e1792ccb59
niketmm <niket.jain@mutualmobile.com>
2022-01-21 11:41:57 +0530

    merged Main

--- app/src/main/java/com/mutualmobile/mmleave/screens/home/HomeScreen.kt
+++ app/src/main/java/com/mutualmobile/mmleave/screens/home/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.core.tween
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
